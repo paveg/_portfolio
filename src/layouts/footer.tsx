3cf65fab17d52d579b0b6d4b1152f070a24a6c28
Ryota Ikezawa <pavegy@gmail.com>
2020-11-14 20:55:11 +0900

    :recycle: Update

---
+++ src/layouts/footer.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 20px;
+  text-align: center;
+  background-color: #222;
+  color: #fff;
+`;
+
+const CopyRight = styled.div`
+  text-align: left;
+`;
+
+const Footer: React.FC = () => {
+  return (
+    <Wrapper>
+      <CopyRight>© 2020 Ryota Ikezawa</CopyRight>
+    </Wrapper>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import MainContainer from '../layouts/main_container';
+import Footer from '../layouts/footer';
 
-const Home: React.FC = () => <MainContainer side={<>profile</>} main={<>writing</>} />;
+const Home: React.FC = () => (
+  <>
+    <MainContainer side={<>profile</>} main={<>writing</>} />
+    <Footer />
+  </>
+);
 
 export default Home;
