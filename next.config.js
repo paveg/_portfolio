5dae90467eb6db8ac3b90609d0d36d73cd69edcb
Ryota Ikezawa <pavegy@gmail.com>
2020-11-16 05:37:15 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     const paths = {
       '/': { page: '/' },
-      '/github': { page: '/github' },
+      '/github': { page: '/' },
     };
     return paths;
   },
