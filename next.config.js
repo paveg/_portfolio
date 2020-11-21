267710b8e46e864575e159c1754aa7977d984a1f
Ryota Ikezawa <pavegy@gmail.com>
2020-11-21 22:27:07 +0900

    :recycle: Fix up next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: false,
 });
 
-module.exports = withBundleAnalyzer({
-  env: {
-    SITE_URL: process.env.SITE_URL,
-  },
-});
+module.exports = withBundleAnalyzer({});
